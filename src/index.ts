feaa88785a90e0f9e730922a145623525c3f7cd6
Andrew Noblet <andrewbnoblet@gmail.com>
2025-09-09 15:59:11 +0000

    Add initial project files, including README, .gitignore, package.json, pnpm-lock.yaml, and index.ts with CSS styles

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import { css } from "lit";
+
+export const index = css`
+  .align-content-space-around {
+    align-content: space-around;
+  }
+
+  .align-items-center {
+    align-items: center;
+  }
+
+  .align-self-center {
+    align-self: center;
+  }
+
+  .align-self-flex-end {
+    align-self: flex-end;
+  }
+
+  .flex-wrap {
+    flex-wrap: wrap;
+  }
+
+  .font-weight-100 {
+    font-weight: 100;
+  }
+
+  .font-weight-200 {
+    font-weight: 200;
+  }
+
+  .font-weight-300 {
+    font-weight: 300;
+  }
+
+  .font-weight-400 {
+    font-weight: 400;
+  }
+
+  .font-weight-500 {
+    font-weight: 500;
+  }
+
+  .font-weight-600 {
+    font-weight: 600;
+  }
+
+  .font-weight-700 {
+    font-weight: 700;
+  }
+
+  .font-weight-800 {
+    font-weight: 800;
+  }
+`;
+
+export default index;
